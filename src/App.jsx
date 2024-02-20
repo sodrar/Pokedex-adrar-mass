5045ea98f460fb7a9eec553d24272f2f70248efb
BSofian <draaboufianso@gmail.com>
2024-02-20 14:14:24 +0100

    Correction of some displaying bugs

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,7 @@ import { PokemonDetails } from './components/PokemonDetails/PokemonDetails'
 
 function App({pokemons}) {
 
-  const [currentId,setCurrentId] = useState(0)
-  const [current,setCurrent] = useState(pokemons[currentId])
+  const [current,setCurrent] = useState(pokemons[0])
 
   let evoImg = "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.pngwing.com%2Fen%2Fsearch%3Fq%3Dnone&psig=AOvVaw07UdkZ894V4LAWT7lWH6cU&ust=1708460797246000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCPDbu8ieuIQDFQAAAAAdAAAAABAI"
   let evolution = {
--- src/components/PokemonDetails/PokemonDetails.jsx
+++ src/components/PokemonDetails/PokemonDetails.jsx
@@ -4,16 +4,26 @@ import './PokemonDetails.css'
 export function PokemonDetails({ pokemon, evolution }) {
     return (
         <div className="details-container">
-            <h3>n°{pokemon.id}</h3>
+            <h3 className='poke-details-number'>n°{pokemon.id}</h3>
             <img className='img-poke-details' src={pokemon.image} alt="image detail du pokemon" />
             <h1>{pokemon.name}</h1>
             <h2>Types</h2>
             <div className='poke-details-type-container'>
                 <img src={pokemon.apiTypes[0].image} className='img-poke-details-type' alt="type 1 du pokemon" />
-                {pokemon.apiTypes.length > 1 ? <img src={pokemon.apiTypes[1].image} className='img-poke-details-type'alt="type 1 du pokemon" /> : null }
+                {pokemon.apiTypes.length > 1 ? <img src={pokemon.apiTypes[1].image} className='img-poke-details-type' alt="type 1 du pokemon" /> : null}
             </div>
-            <h3>Evolution</h3>
-            <Pokemon className="poke-details-evo" pokemon={evolution} />
+
+            {
+                pokemon.apiEvolutions.length > 0 ?
+                    <div>
+                        <h3>Evolution</h3>
+                        <Pokemon className="poke-details-evo" pokemon={evolution} />
+                    </div>
+                    :
+                    <h3>Pas d'évolution</h3>
+            }
+
+
         </div>
     )
 }
